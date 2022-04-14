681c8cce50e1b61604f5c3fdfa8bbb894b0726d5
WiiruxCruz <hialf.92@gmail.com>
2022-04-14 01:17:22 -0500

    Configuración opcional al backend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,7 +21,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-	  useHash: true
+	//useHash: true //si se realiza esto no se requiere configuración en el backeng
+	useHash: false
   })],
   exports: [RouterModule]
 })
